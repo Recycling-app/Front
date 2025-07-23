d7ad8b47d65f6c4a722bd180339967abaed15370
mingki <myong1306@gmail.com>
2025-07-23 21:34:37 +0900

    [feat] : 카메라 버튼 기능 - 버튼 누르면 카메라 실행

--- app/src/main/java/com/example/recycling_app/MainActivity.java
+++ app/src/main/java/com/example/recycling_app/MainActivity.java
@@ -1,44 +1,25 @@
 package com.example.recycling_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageButton;
 
-import com.gun0912.tedpermission.PermissionListener;
-import com.gun0912.tedpermission.TedPermission;
-
-import java.util.List;
-
-// MainActivity 클래스는 앱의 메인 화면을 담당합니다.
 public class MainActivity extends AppCompatActivity {
 
-    // 액티비티가 처음 생성될 때 호출되는 메서드입니다.
+    // 카메라 버튼 누르면 카메라 실행
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // 부모 클래스의 onCreate 메서드를 호출합니다.
-
-        // start.xml 레이아웃 파일을 이 액티비티의 화면으로 설정합니다.
-        // R.layout.start는 res/layout/start.xml 파일을 참조합니다.
-        setContentView(R.layout.start);
-
-        PermissionListener permissionListener = new PermissionListener() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cameraicon);
+        ImageButton cameraButton = findViewById(R.id.camera_button_icon);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPermissionGranted() {
-                Toast.makeText(getApplicationContext(), "권한이 허용됨", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
             }
-
-            @Override
-            public void onPermissionDenied(List<String> deniedPermissions) {
-                Toast.makeText(getApplicationContext(), "권한이 거부됨", Toast.LENGTH_SHORT).show();
-            }
-        };
-        TedPermission.with(getApplicationContext())
-                .setPermissionListener(permissionListener)
-                .setRationaleMessage("카메라 권한이 필요합니다.")
-                .setDeniedMessage("거부하셨습니다.")
-                .setPermissions(Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.CAMERA)
-                .check();
+        });
     }
 }
