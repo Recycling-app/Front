e01fbbbba08e6928b9d06d6dc5f82a673eface1c
mingki <myong1306@gmail.com>
2025-07-24 00:05:30 +0900

    [feat] : 카카오맵 api 연동을 위한 설정 - 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,11 @@ android {
 }
 
 dependencies {
+    implementation("com.kakao.maps.open:android:2.12.8")
+    implementation("com.kakao.sdk:v2-common:2.19.0")
     implementation("io.github.ParkSangGwon:tedpermission:2.3.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.kakao.maps.open:android:2.12.8")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -52,6 +55,9 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.exifinterface)
+    implementation(libs.firebase.firestore)
+    implementation(libs.androidx.media3.common)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
