4bed059ebfd8d082f5fd1ce15aba87ccdbe3c95c
mingki <myong1306@gmail.com>
2025-07-25 13:38:26 +0900

    refactor : 사용하지 않는 코드 삭제

--- app/src/main/java/com/example/recycling_app/MainActivity.java
+++ app/src/main/java/com/example/recycling_app/MainActivity.java
@@ -8,18 +8,5 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 카메라 버튼 누르면 카메라 실행
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.cameraicon);
-        ImageButton cameraButton = findViewById(R.id.camera_button_icon);
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+    // 시작화면
 }
