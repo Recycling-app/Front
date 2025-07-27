97975e8f1af909eb66408f5c2b92c77dda61a8c5
mingki <myong1306@gmail.com>
2025-07-27 22:43:21 +0900

    "Resolve merge conflicts"

--- app/src/main/java/com/example/recycling_app/MainscreenActivity.java
+++ app/src/main/java/com/example/recycling_app/MainscreenActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
