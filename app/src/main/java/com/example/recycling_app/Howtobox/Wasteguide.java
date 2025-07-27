352452ca71bc47ac1ba8ab7bb9807446945a3673
mingki <myong1306@gmail.com>
2025-07-28 04:59:41 +0900

    [feat] : 지역별 가이드 기능 추가 - 등록 및 터치하면 기본 디자인 출력

--- app/src/main/java/com/example/recycling_app/Howtobox/Wasteguide.java
+++ app/src/main/java/com/example/recycling_app/Howtobox/Wasteguide.java
@@ -1,4 +1,44 @@
 package com.example.recycling_app.Howtobox;
 
-public class Wasteguide {
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.recycling_app.R;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Wasteguide extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wasteguide);
+
+    }
 }
